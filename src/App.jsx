f0354bdf32e221ecfb776c04064f417766b65d92
Victor Koome <koomev786@gmail.com>
2024-10-09 15:39:07 +0300

    configure styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import styled from "styled-components"
+
+const H1 = styled.h1`
+  background-color: red;
+  font-size: 20px;
+`;
 function App() {
   return (
     <div>
-      Hello
+      <H1>Hello world</H1>
     </div>
   )
 }
