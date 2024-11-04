a2d5dc8408f0b65030d4163fc815617a6b200b18
Victor Koome <koomev786@gmail.com>
2024-11-04 12:14:21 +0300

    storing images in buckets

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export default async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
@@ -12,16 +12,33 @@ export default async function getCabins() {
 }
 
 export async function createCabin(newCabin) {
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll("/", "");
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+  //1. Create Cabin
   const { data, error } = await supabase
     .from("cabins")
-    .insert([newCabin])
+    .insert([{ ...newCabin, image: imagePath }])
     .select();
-    
+
   if (error) {
     console.error(error);
     throw new Error("Cabin could not be created!");
   }
 
+  //2. Upload Image
+  const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .upload(imageName, newCabin.image);
+
+  //3. Delete Cabin If there was an error uploading the image
+  if (storageError) {
+    await supabase.from("cabins").delete().eq("id", data.id);
+    console.error(storageError);
+    throw new Error(
+      "Cabin image could not be uploaded and the cabin was not created"
+    );
+  }
   return data;
 }
 
